550f8972b46633c5bf3059a8b8aefd5eb1ab9489
Adam Wulf <adam.wulf@gmail.com>
2020-08-11 00:41:04 -0500

    Adding basics for PathElement

--- Sources/DrawUI/Path Helpers/DrawnStroke.swift
+++ Sources/DrawUI/Path Helpers/DrawnStroke.swift
@@ -10,7 +10,32 @@ import CoreGraphics
 
 class DrawnStroke {
 
-    init(with tool: Pen) {
+    let identifier: String
+    var path: UIBezierPath?
+    var borderPath: UIBezierPath?
+    private(set) var segments: [PathElement]
+    private var eventIdToSegment: [String: PathElement]
+    private var waitingEvents: [TouchStreamEvent]
+    let tool: Pen
 
+    /// The first event that created this stroke. the [event touchIdentifier] can be useful for mapping this stroke to a [UITouch identifier]
+    private(set) var event: TouchStreamEvent?
+
+    /// Used by renderers to determine when a stroke was last updated
+    let version: UInt
+
+    private let smoother: SegmentSmoother
+    private let savedSmoother: SegmentSmoother
+
+    init(with tool: Pen) {
+        identifier = UUID().uuidString
+        self.tool = tool
+        segments = []
+        smoother = SegmentSmoother()
+        savedSmoother = smoother
+        eventIdToSegment = [:]
+        waitingEvents = []
+        version = 0
+        event = nil
     }
 }
